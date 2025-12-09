b84e77953911f4a2e9cb1d21916e2817087dc3d9
kajumac07 <kajumac13@gmail.com>
2025-12-09 15:04:47 +0530

    google verification added

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "avainfotek.com - Made By Three",
   description: "Built by three, made for you",
+  verification: {
+    google: "yVLfDgVy_7d_92A",
+  },
 };
 
 export default function RootLayout({
