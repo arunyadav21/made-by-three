7030229e6f2d5f4914ba27adc477de14fdeb9f2f
Arun Yadav <arun305565@gmail.com>
2025-12-09 15:57:04 +0530

    issue fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
   title: "avainfotek.com - Made By Three",
   description: "Built by three, made for you",
   verification: {
-    google: "yVLfDgVy_7d_92A", 
+    google: "yVLfDgVy_7d_92APseEOxrLODa9_KQNmtl8gGP99F7s", 
   },
 };
 // -------------------------------------------------------
